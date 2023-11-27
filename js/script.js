5d0c476a9ff6372f030ef7b2e6fba1d51448b389
MarcoPicca <il96marco@gmail.com>
2023-11-27 11:41:30 +0100

    array auto

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Crea un array composto da 10 automobili.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+// Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+// Infine stampa separatamente i 3 array.
+
+
+
+
+let arrayAutomobili = [
+    { marca: fiat, modello: corolla, alimentazione: benzina },
+    { marca: wolksvagen, modello: modello2, alimentazione: diesel },
+    { marca: fiat, modello: modello3, alimentazione: gpl },
+    { marca: fiat, modello: modello4, alimentazione: metano },
+    { marca: fiat, modello: modello5, alimentazione: elettrico },
+    { marca: fiat, modello: modello6, alimentazione: benzina },
+    { marca: fiat, modello: modello7, alimentazione: metano },
+    { marca: fiat, modello: modello8, alimentazione: diesel },
+    { marca: fiat, modello: modello9, alimentazione: gpl },
+    { marca: fiat, modello: modello10, alimentazione: benzina }
+];
