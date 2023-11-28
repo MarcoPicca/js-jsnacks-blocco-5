8a0ccdf3fccb15b03a240adf9f3448a41899e3bf
MarcoPicca <il96marco@gmail.com>
2023-11-28 12:32:30 +0100

    tentativo snak 4

---
+++ snack-4/js/script.js
@@ -0,0 +1,18 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
+// prendendo alternativamente gli elementi da uno e dall'altro
+
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const arrayNum = [0, 1, 2, 3, 4, 5, 6];
+const arrayWord = ["sole", "mare", "fisarmonica", "spatola", "corvette", "filo da pesca", "coriandoli"];
+
+
+
+if( arrayNum.length !== arrayWord.length){
+    alert('no');
+} else {
+    let arrayEmpty = [];
+}
+
+
+
